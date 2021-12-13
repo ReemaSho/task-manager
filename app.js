f6b517709784ca08a6baa332f491b7cadd251428
Reema <Reemaalshohof88@gmail.com>
2021-12-13 16:12:52 +0100

    set up the controllers and routes folders and connect them with app.js

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 import express from "express";
 const app = express();
-
-const port = 3000;
-
+import tasks from "./routes/tasks.js";
+//json middleware
+app.use(express.json());
 //routes
 app.get("/hello", (req, res) => {
   res.send("task manager app");
 });
+
+app.use("/api/v1/tasks", tasks);
+
+const port = 3000;
 app.listen(port, console.log(`Server is listening on port ${port}...`));
