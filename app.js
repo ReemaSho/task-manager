449d8302ecb29a14cef2d0337b9584fa16434795
Reema <Reemaalshohof88@gmail.com>
2021-12-14 20:59:47 +0100

    connecting my app to mongooseDB..

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 import express from "express";
 const app = express();
 import tasks from "./routes/tasks.js";
+import mongooseDB from "./db/connect.js";
+import { config } from "dotenv";
+const DB_URI = config();
 //json middleware
 app.use(express.json());
 //routes
@@ -9,6 +12,14 @@ app.get("/hello", (req, res) => {
 });
 
 app.use("/api/v1/tasks", tasks);
-
 const port = 3000;
-app.listen(port, console.log(`Server is listening on port ${port}...`));
+//connect DB
+const start = async () => {
+  try {
+    await mongooseDB(DB_URI.parsed.MONGO_URI);
+    app.listen(port, console.log(`Server is listening on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
