c3f0a140093e666b6111c5e5b7bbfad957ead1ab
Reema <Reemaalshohof88@gmail.com>
2021-12-19 21:49:59 +0100

    post request for creating new task, add the required css html for the Form

--- public/browser-app.js
+++ public/browser-app.js
@@ -3,6 +3,7 @@ const loadingDOM = document.querySelector(".loading-text");
 const formDOM = document.querySelector(".task-form");
 const taskInputDOM = document.querySelector(".task-input");
 const formAlertDOM = document.querySelector(".form-alert");
+const newTaskBtnDOM = document.querySelector(".new-task-btn");
 
 const createTaskDOMEle = (completed, name, taskID) => {
     return `<div class="single-task ${completed && "task-completed"}">
@@ -28,24 +29,20 @@ const showTasks = async() => {
         const request = await fetch("/api/v1/tasks");
         const data = await request.json();
 
-        console.log(data.tasks);
         if (data.tasks.length < 1) {
-            tasksDOM.classList.remove("display-none");
+            tasksDOM.innerHTML = ` <h5 class="empty-list">No tasks in your list</h5>`;
             loadingDOM.style.visibility = "hidden";
             return;
         }
         const allTasks = data.tasks
             .map((task) => {
                 const { completed, _id: taskID, name } = task;
-                console.log(completed, taskID, name);
                 return createTaskDOMEle(completed, name, taskID);
             })
             .join("");
-        console.log(allTasks);
+
         loadingDOM.style.visibility = "hidden";
-        tasksDOM.classList.remove("display-none");
         tasksDOM.innerHTML = allTasks;
-        console.log(tasksDOM);
     } catch (error) {
         tasksDOM.innerHTML =
             '<h5 class="empty-list">There was an error, please try later....</h5>';
@@ -58,11 +55,9 @@ showTasks();
 
 tasksDOM.addEventListener("click", async(e) => {
     const ele = e.target;
-    console.log(ele);
     if (ele.parentElement.classList.contains("delete-btn")) {
         loadingDOM.style.visibility = "visible";
         const id = ele.parentElement.dataset.id;
-        console.log(ele.parentElement.dataset.id);
         try {
             await fetch(`/api/v1/tasks/${id}`, {
                 method: "DELETE",
@@ -74,4 +69,46 @@ tasksDOM.addEventListener("click", async(e) => {
         }
     }
     loadingDOM.style.visibility = "hidden";
+});
+
+// form
+const showForm = () => {
+    newTaskBtnDOM.classList.add("display-none");
+    formDOM.classList.remove("display-none");
+};
+
+newTaskBtnDOM.addEventListener("click", showForm);
+formDOM.addEventListener("submit", async(e) => {
+    e.preventDefault();
+    const taskName = taskInputDOM.value;
+    const options = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ name: taskName }),
+    };
+    console.log(options);
+    try {
+        const response = await fetch("/api/v1/tasks", options);
+        console.log(response);
+
+        showTasks();
+        if (response.status === 201) {
+            taskInputDOM.value = "";
+            formAlertDOM.style.display = "block";
+            formAlertDOM.textContent = `success, task added`;
+            formAlertDOM.classList.add("text-success");
+        } else {
+            formAlertDOM.style.display = "block";
+
+            formAlertDOM.textContent = `error, please try again`;
+        }
+    } catch (error) {
+        console.log(error);
+    }
+    setTimeout(() => {
+        formAlertDOM.style.display = "none";
+        formAlertDOM.classList.remove("text-success");
+        newTaskBtnDOM.classList.remove("display-none");
+        formDOM.classList.add("display-none");
+    }, 4000);
 });
